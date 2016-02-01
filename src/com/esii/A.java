59abaf1f134ca9d1f536370c318e4df7d5202d78
Carlos Pereira <carloshpereira27@gmail.com>
2016-02-01 07:24:44 -0200

    First commit

---
+++ src/com/esii/A.java
@@ -0,0 +1,24 @@
+package com.esii;
+
+public class A {
+	
+	private int A1;
+	private float A2;
+	
+	public int getA1() { 
+		return this.A1; 
+	}
+	
+	public float getA2() { 
+		return this.A2; 
+	}
+	
+	public void setA1(int A1) { 
+		this.A1 = A1; 
+	}
+	
+	public void setA2(float A2) { 
+		this.A2 = A2; 
+	}
+	
+}
