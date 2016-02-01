a129a74f961076d0195806b720ff0ceeadc321a0
Carlos Pereira <carloshpereira27@gmail.com>
2016-02-01 18:53:01 -0200

    Fifth commit - Aprinc4

--- src/com/esii/A.java
+++ src/com/esii/A.java
@@ -5,7 +5,8 @@ public class A {
 	private int A1;
 	private float A2;
 	private String A3;
-	
+	private String Aprinc4;
+
 	public int getA1() { 
 		return this.A1; 
 	}
@@ -17,6 +18,10 @@ public class A {
 	public String getA3() { 
 		return this.A3; 
 	}
+
+	public String getAprinc4() { 
+		return this.Aprinc4; 
+	}
 	
 	public void setA1(int A1) { 
 		this.A1 = A1; 
@@ -24,6 +29,10 @@ public class A {
 	
 	public void setA3(String A3) { 
 		this.A3 = A3; 
+	}
+
+	public void setAprinc4(String Aprinc4) { 
+		this.Aprinc4 = Aprinc4; 
 	}
 	
 	public float soma() {
