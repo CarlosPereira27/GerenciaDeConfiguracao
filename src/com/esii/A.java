535ec72aaf5ddbd2f385b7d5f320ff1b9914e978
Carlos Pereira <carloshpereira27@gmail.com>
2016-02-01 09:14:13 -0200

    Second Commit

--- src/com/esii/A.java
+++ src/com/esii/A.java
@@ -4,6 +4,7 @@ public class A {
 	
 	private int A1;
 	private float A2;
+	private String A3;
 	
 	public int getA1() { 
 		return this.A1; 
@@ -13,12 +14,16 @@ public class A {
 		return this.A2; 
 	}
 	
+	public String getA3() { 
+		return this.A3; 
+	}
+	
 	public void setA1(int A1) { 
 		this.A1 = A1; 
 	}
 	
-	public void setA2(float A2) { 
-		this.A2 = A2; 
+	public void setA3(String A3) { 
+		this.A3 = A3; 
 	}
-	
+			
 }
