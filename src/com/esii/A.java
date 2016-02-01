815227d95d8e2d62a48f44bc74f840dce314c47a
Carlos Pereira <carloshpereira27@gmail.com>
2016-02-01 15:18:09 -0200

    Fourth commit - Aramo4

--- src/com/esii/A.java
+++ src/com/esii/A.java
@@ -5,6 +5,7 @@ public class A {
 	private int A1;
 	private float A2;
 	private String A3;
+	private String Aramo4;
 	
 	public int getA1() { 
 		return this.A1; 
@@ -18,12 +19,20 @@ public class A {
 		return this.A3; 
 	}
 	
+	public String getAramo4() { 
+		return this.Aramo4; 
+	}
+	
 	public void setA1(int A1) { 
 		this.A1 = A1; 
 	}
 	
 	public void setA3(String A3) { 
 		this.A3 = A3; 
+	}
+	
+	public void setAramo4(String Aramo4) { 
+		this.Aramo4 = Aramo4; 
 	}
 	
 	public float soma() {
