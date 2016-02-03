1715fbb179d1b5ca39619a00b8940a947178e118
Carlos Pereira <carloshpereira27@gmail.com>
2016-02-03 07:18:21 -0200

    Forçar merge

--- src/com/esii/B.java
+++ src/com/esii/B.java
@@ -4,6 +4,7 @@ public class B {
 	
 	private int B1;
 	private float B2;
+	private char B3;
 	
 	public int getB1() {
 		return this.B1;
@@ -13,12 +14,20 @@ public class B {
 		return this.B2;
 	}
 	
+	public char getB3() {
+		return this.B3;
+	}
+	
 	public void setB1(int B1) {
 		this.B1 = B1;
 	}
 	
 	public void setB2(float B2) {
 		this.B2 = B2;
+	}
+	
+	public void setB3(char B3) {
+		this.B3 = B3;
 	}
 	
 }
