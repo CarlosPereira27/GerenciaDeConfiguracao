12a97b87907052eb10d21c3fd79c148c8b18b64c
Carlos Pereira <carloshpereira27@gmail.com>
2016-02-03 06:20:49 -0200

    Sixth commit - Class B

---
+++ src/com/esii/B.java
@@ -0,0 +1,24 @@
+package com.esii;
+
+public class B {
+	
+	private int B1;
+	private float B2;
+	
+	public int getB1() {
+		return this.B1;
+	}
+	
+	public float getB2() {
+		return this.B2;
+	}
+	
+	public void setB1(int B1) {
+		this.B1 = B1;
+	}
+	
+	public void setB2(float B2) {
+		this.B2 = B2;
+	}
+	
+}
